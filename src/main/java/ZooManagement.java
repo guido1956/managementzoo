48b7634c2bca4071f1b06c1f0c99748b7d16d075
Guido1956 <GuidoDulos@valori.nl>
2020-12-17 16:16:40 +0100

    Hoofdstuk 2 -voor practicum- print statement

--- src/main/java/ZooManagement.java
+++ src/main/java/ZooManagement.java
@@ -1,8 +1,10 @@
 public class ZooManagement {
     public static void main(String[] args) {
-
+        system.out.println("Hello World");
+        System.out.println("This is my zoo");
     }
 }
+
 
 
 
