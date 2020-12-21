2062307b0c7f755cf4750a37d338aa7c57099e9a
Guido1956 <GuidoDulos@valori.nl>
2020-12-21 12:09:46 +0100

    Hoofdstuk 2 -voor practicum- print statement

--- src/main/java/ZooManagementHoofdstuk3.java
+++ src/main/java/ZooManagementHoofdstuk3.java
@@ -1,16 +1,10 @@
 public class ZooManagementHoofdstuk3 {
-    /**
-     * ZooManagementHoofdstuk3
-     * Demo voor werken met Strings - ints - doubles
-     * Demo voor gebruiken van System.out.println - System.out.print - System.out.printf
-     */
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         // Werken met Strings en printstatements
         System.out.println("Onze tijger heet Zigro");   //literal + newline
         System.out.print("Onze tijger heet ");          //literal zonder newline
         System.out.println("Zigro");                    //literal met newline
         System.out.printf("%s %s\n", "Onze tijger heet" , "Zigro");   //String met subsituut %s en argumenten
-
         //Werken met ints en printstatements
         System.out.print("Onze tijger is ");
         System.out.print(12);
@@ -19,7 +13,6 @@ public class ZooManagementHoofdstuk3 {
         System.out.print(12 * 12);
         System.out.println(" maanden");
         System.out.printf("Onze tijger is %d jaar oud\nDat is %d maanden", 12, 12*12);
-
         //Werken met doubles en printstatements
         System.out.println("Onze tijger is ");
         System.out.print(12.5);
